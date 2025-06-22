354c77f3db6e17fb69e81ca1df9761f75556d7c5
ankit <ankitjha0723@gmail.com>
2025-06-22 10:26:41 +0530

    1st Project setup

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
+const heading = React.createElement("h1", {id: "heading", className: "headingClass"
+},"Hello World");
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading); 
+
+
+//  <div id="parent">
+//         <div id="child">
+//             <h1>I m h1</h1>
+//         </div>
+// </div>
+
+const parent = React.createElement("div",{id: "parent"},
+      React.createElement("div",{id: "child"},
+        React.createElement("h1",{},"I m h1"),
+        React.createElement("h1",{},"Ankit")
+      )
+);
+
+
+//  const root = ReactDOM.createRoot(document.getElementById("hello"));
+//  root.render(parent); 
