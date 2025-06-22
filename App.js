ffc0806ca2eadc0410cdcdd73fca9421dc39632f
ankit <ankitjha0723@gmail.com>
2025-06-22 10:32:39 +0530

    made code cleaner

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ const heading = React.createElement("h1", {id: "heading", className: "headingCla
 //         </div>
 // </div>
 
+
+
 const parent = React.createElement("div",{id: "parent"},
       React.createElement("div",{id: "child"},
         React.createElement("h1",{},"I m h1"),
---
+++ dist/React.7164bdae.js
@@ -0,0 +1,2 @@
+let heading=React.createElement("h1",{id:"heading",className:"headingClass"},"Hello World"),parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I m h1"),React.createElement("h1",{},"Ankit"))),root=ReactDOM.createRoot(document.getElementById("hello"));root.render(parent);
+//# sourceMappingURL=React.7164bdae.js.map
